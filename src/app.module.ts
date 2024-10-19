b51dcccf9f518644846e49b882ccbfafc070989b
choewy <choewy32@gmail.com>
2024-10-19 23:52:45 +0900

    core: app.module:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigFactoryModule, TypeOrmConfigFactory } from './common';
+import { ContextModule, ContextQueryLogger, LoggerModule } from './core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigFactoryModule.forRoot(),
+    ContextModule.forRoot(),
+    LoggerModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      inject: [TypeOrmConfigFactory, ContextQueryLogger],
+      useFactory(configFactory: TypeOrmConfigFactory, contextQueryLogger: ContextQueryLogger) {
+        return configFactory.createTypeOrmModuleOptions(contextQueryLogger);
+      },
+    }),
+  ],
 })
 export class AppModule {}
