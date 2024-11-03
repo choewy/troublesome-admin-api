1437c1ac7a6561055d941e15c4c5ed6d5e50d1ec
choewy <choewy32@gmail.com>
2024-11-03 20:06:33 +0900

    chore: app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+import { ConfigKey } from './constant/enums';
+import { getEnv } from './constant/helpers';
+
 @Injectable()
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   public getAppProfile() {
     return {
-      name: this.configService.get('npm_package_name'),
-      version: this.configService.get('npm_package_version'),
+      name: this.configService.get(ConfigKey.NpmPackageName),
+      version: this.configService.get(ConfigKey.NpmPackageVersion),
+      env: getEnv(),
     };
   }
 }
