238800e5780d98a64cd9272f728f2ca7b7e7f8ea
choewy <choewy32@gmail.com>
2024-11-03 20:07:31 +0900

    chore: app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
+import { Public } from './constant/decorators';
 
+@Public()
 @ApiTags('앱')
 @Controller()
 export class AppController {
