f066bd17f772855ca27a46cf6a2c4cfd61c0d343
choewy <choewy@aininetworks.com>
2024-11-13 16:54:00 +0900

    feature: purchaser partner

--- src/application/purchaser/dto/purchaser.dto.ts
+++ src/application/purchaser/dto/purchaser.dto.ts
@@ -2,12 +2,17 @@ import { ApiResponseProperty } from '@nestjs/swagger';
 
 import { Purchaser } from '../purchaser.entity';
 
+import { PartnerDTO } from '@/application/partner/dto/patner.dto';
+
 export class PurchaserDTO {
   @ApiResponseProperty({ type: BigInt })
   id: string;
 
   @ApiResponseProperty({ type: String })
   name: string;
+
+  @ApiResponseProperty({ type: PartnerDTO })
+  partner: PartnerDTO | null;
 
   @ApiResponseProperty({ type: Date })
   createdAt: Date;
@@ -18,6 +23,7 @@ export class PurchaserDTO {
   constructor(purchaser: Purchaser) {
     this.id = purchaser.id;
     this.name = purchaser.name;
+    this.partner = purchaser.partner ? new PartnerDTO(purchaser.partner) : null;
     this.createdAt = purchaser.createdAt;
     this.updatedAt = purchaser.updatedAt;
   }
